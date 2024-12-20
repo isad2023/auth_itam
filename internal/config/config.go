7032a7fc90f4fcff49062660fac6767e1fc607bf
Kin <dkonopliannikov05@gmail.com>
2024-12-20 15:19:17 +0300

    add read config file

---
+++ internal/config/config.go
@@ -0,0 +1,61 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type AppConfig struct {
+	DBUser         string
+	DBPass         string
+	DBHost         string
+	DBPort         string
+	DBName         string
+	MigrationsPath string
+}
+
+func LoadConfig() (*AppConfig, error) {
+
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("Error getting current working directory: %v", err)
+	}
+	fmt.Println("Current working directory:", dir)
+
+	if err := godotenv.Load(); err != nil {
+		log.Printf("Warning: Could not load .env file: %v", err)
+	}
+
+	config := &AppConfig{
+		DBUser:         os.Getenv("DB_USER"),
+		DBPass:         os.Getenv("DB_PASSWORD"),
+		DBHost:         os.Getenv("DB_HOST"),
+		DBPort:         os.Getenv("DB_PORT"),
+		DBName:         os.Getenv("DB_NAME"),
+		MigrationsPath: os.Getenv("MIGRATIONS_PATH"),
+	}
+
+	// Проверяем обязательные параметры
+	missingVars := []string{}
+	if config.DBUser == "" {
+		missingVars = append(missingVars, "DB_USER")
+	}
+	if config.DBPass == "" {
+		missingVars = append(missingVars, "DB_PASSWORD")
+	}
+	if config.DBName == "" {
+		missingVars = append(missingVars, "DB_NAME")
+	}
+	if config.MigrationsPath == "" {
+		missingVars = append(missingVars, "MIGRATIONS_PATH")
+	}
+
+	if len(missingVars) > 0 {
+		return nil, fmt.Errorf("missing required environment variables: %v", missingVars)
+	}
+
+	return config, nil
+}
