5769b0e391f0eb90045b4ea0e6c63ff28f690cb4
Kin <dkonopliannikov05@gmail.com>
2024-12-20 15:20:00 +0300

    add connect db and read config in main.go

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"itam_auth/internal/config"
+	"itam_auth/internal/database"
 	"itam_auth/internal/routes"
+	"log"
 )
 
 // @title LiveCode API
@@ -11,6 +14,21 @@ import (
 // @host localhost:8080
 // @BasePath /api
 func main() {
+	appConfig, err := config.LoadConfig()
+	if err != nil {
+		log.Fatalf("Failed to load configuration: %v", err)
+	}
+	log.Println("Configuration successfully loaded.")
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		appConfig.DBHost, appConfig.DBPort, appConfig.DBUser, appConfig.DBPass, appConfig.DBName)
+
+	if err := database.Initialize(dsn); err != nil {
+		log.Fatalf("Failed to initialize database: %v", err)
+	}
+	defer database.Close()
+	log.Println("Database successfully connected.")
+
 	router := routes.SetupRoutes()
 
 	fmt.Println("Starting server on port 8080")
